186ee4e34b354e7cfeb762c4e286ba074dd1186a
veronicapava <veronicapava@gmail.com>
2022-02-10 18:08:15 -0500

    Se crea la pelota y se crea la funcion clean

--- script.js
+++ script.js
@@ -24,6 +24,22 @@
     }
 })();
 
+//movimiento de pelota
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+
+    }
+})();
+
 //Pelota
 (function () {
     self.Bar = function (x, y, width, height, board) {
@@ -67,51 +83,69 @@
     }
 
     self.BoardView.prototype = {
-
+        clean: function () { //limpiar el canvas
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function () {
             for (var i = this.board.elements.length - 1; i >= 0; i--) { // Se corre el elemento para dibujarlo
                 var ele = this.board.elements[i]
                 draw(this.ctx, ele)
-            }
+            };
+        },
+        play: function () { // para que el juego funcione
+            this.clean();
+            this.draw();
         }
     }
 
     //funcion para dinujar los elementos
 
     function draw(ctx, element) {
-        if (element !== null && element.hasOwnProperty("kind")) {
-            switch (element.kind) {
-                case "rectangle":
-                    //fillRect es una funcion del contexto que nos permite dibujar un cuadrado, recib los parametros
-                    ctx.fillRect(element.x, element.y, element.width, element.height)
-                    break;
-            }
+        switch (element.kind) {
+            case "rectangle":
+                //fillRect es una funcion del contexto que nos permite dibujar un cuadrado, recib los parametros
+                ctx.fillRect(element.x, element.y, element.width, element.height)
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
-
     }
 })();
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
-var bar = new Bar(700, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board)
+
+
+//animacion
+window.requestAnimationFrame(controller);
+
+
 
 document.addEventListener("keydown", function (ev) {
-    console.log(ev.keyCode);
-    if (ev.keyCode == 38) {
+    ev.preventDefault();
+    if (ev.keyCode == 38) { //movimiento de la felcha arriba y abajo 
         bar.up();
     } else if (ev.keyCode == 40) {
         bar.down();
+    } else if (ev.keyCode == 87) { //movimiento de la w y s
+        bar_2.up();
+    } else if (ev.keyCode == 83) {
+        bar_2.down();
     }
-    console.log("" + bar)
-
+    console.log("" + bar_2)
 })
 
-self.addEventListener("load", main)
 
 //Ejecutar todos los elementos 
-function main() {
-
-    board_view.draw();
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
 };
 
